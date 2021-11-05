5e27d206598f7d0fc5b1c6fed7d4529c6a4a9481
Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2021-11-05 17:32:07 +0700

    add images to assets

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String bgPlash = 'assets/images/splash.png';
   static const String logo = 'assets/images/logo.png';
+  static const String icBack = 'assets/images/back.png';
 }
